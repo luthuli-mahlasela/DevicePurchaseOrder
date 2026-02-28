5a3431684d0731aa334e19a3d177356c5a0456cb
Luthuli Mahlasela <luthulim@thekwini.edu.za>
2026-02-28 08:08:22 +0200

    Added WebElement for DropDownList and Selector

--- src/test/java/LoginPageNdosi.java
+++ src/test/java/LoginPageNdosi.java
@@ -17,22 +17,35 @@ public class LoginPageNdosi {
         driver.findElement(By.id("login-email")).sendKeys("dust.attack@gmail.com");
         driver.findElement(By.id("login-password")).sendKeys("@12345678");
         driver.findElement(By.id("login-submit")).click();
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/button/span[2]")).click();
         driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/div/button[2]/span[2]")).click();
         driver.findElement(By.xpath("//*[@id=\"tab-btn-web\"]/span[2]")).click();
         //driver.findElement(By.id("deviceType")).click();
     WebElement ddown= driver.findElement(By.xpath("//*[@id=\"deviceType\"]"));
-    Select select= new Select(ddown);
-    select.selectByIndex(0);
-       driver.findElement(By.xpath("//*[@id=\"brand\"]")).click();
-       select.selectByIndex(1);
+    Select selectDevice= new Select(ddown);
+    selectDevice.selectByIndex(1);
+
+    WebElement dropD=driver.findElement(By.xpath("//*[@id=\"brand\"]"));
+    Select selectBrand= new Select(dropD);
+    selectBrand.selectByIndex(1);
+
        driver.findElement(By.xpath("//*[@id=\"inventory-form-grid\"]/div[4]/div/label[2]")).click();
-       select.selectByIndex(1);
-       driver.findElement(By.id("color")).click();
-       select.selectByIndex(1);
-       driver.findElement(By.id("quantity")).sendKeys("2");
+       selectDevice.selectByIndex(1);
+
+       WebElement dropDown=driver.findElement(By.xpath("//*[@id=\"color\"]"));
+       Select selectColor= new Select(dropDown);
+       selectColor.selectByIndex(1);
+
+       WebElement numericInput = driver.findElement(By.xpath("//*[@id=\"quantity\"]"));
+       numericInput.clear();
+       numericInput.sendKeys("2");
+       //driver.findElement(By.id("quantity")).sendKeys("2");
+
+    driver.findElement(By.xpath("//*[@id=\"address\"]")).sendKeys("123 Mayfair");
+    driver.findElement(By.id("inventory-next-btn")).click();
+
 
 
 
