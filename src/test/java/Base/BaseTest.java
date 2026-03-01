24ce95025f01a4abef34b387e28b26e6de3aff63
Luthuli Mahlasela <luthulim@thekwini.edu.za>
2026-03-01 19:53:01 +0200

    Changes on BaseTest

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -5,6 +5,7 @@ import Pages.InventoryPage;
 import Pages.LoginPage;
 import Utilities.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,16 +15,16 @@ public class BaseTest {
     public final String browserChoice="chrome";
 
      WebDriver driver;
-    public LoginPage loginPage;
-    public InventoryPage inventoryPage;
-    public ExtrasPage extrasPage;
+    public LoginPage loginPage= PageFactory.initElements(driver, LoginPage.class);
+    public InventoryPage inventoryPage=PageFactory.initElements(driver,InventoryPage.class);
+    public ExtrasPage extrasPage=PageFactory.initElements(driver, ExtrasPage.class);
 
     @BeforeMethod
     public void setUp() {
         driver = browserFactory.startBrowser(browserChoice,url);
-        loginPage= new LoginPage(driver);
-        inventoryPage= new InventoryPage(driver);
-        extrasPage= new ExtrasPage(driver);
+        //loginPage= new LoginPage(driver);
+       // inventoryPage= new InventoryPage(driver);
+        //extrasPage= new ExtrasPage(driver);
 
     }
     @AfterMethod
